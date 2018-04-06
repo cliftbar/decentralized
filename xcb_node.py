c60bbc97660f594b30de88523169f9b811c0e3eb
cliftbar <cwbarclift@gmail.com>
2018-04-05 17:54:56 -0700

    cleaning up a little bit

--- xcb_node.py
+++ xcb_node.py
@@ -4,168 +4,6 @@ from threading import Thread
 import time
 
 class Node:
-    # peer store class to hold all peers of the node
-    class p2p:
-        peers = set()
-        clients = {}
-
-        @classmethod
-        def addPeer(self, peer):
-            self.peers.add(peer)
-        @classmethod
-        def removePeer(self, peer):
-            self.peers.remove(peer)
-
-    class Server:
-        def accept_incoming_connections(self):
-            """Sets up handling for incoming clients."""
-            while True:
-                client, client_address = self.SERVER.accept()
-                print("%s:%s has connected." % client_address)
-                # send peer list with every welcome message, to get new client up to date
-                client.send(bytes("Greetings from the cave! Now type your name and press enter!|Peers;{0}".format(",".join(Node.p2p.peers)), "utf8"))
-                # add peer to peer store
-                print(client_address)
-                #Node.p2p.addPeer(client_address[0] + ":" + str(client_address[1]))
-                Thread(target=self.handle_client, args=(client,)).start()
-
-        def handle_client(self, client):  # Takes client socket as argument.
-            """Handles a single client connection."""
-
-            name = client.recv(self.BUFSIZ).decode("utf8")
-            welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
-            client.send(bytes(welcome, "utf8"))
-            msg = "%s has joined the chat!" % name
-            print("server: " + msg)
-            self.broadcast(bytes(msg, "utf8"))
-            self.broadcast_peers(bytes(msg, "utf8"))
-            Node.p2p.clients[client] = name
-
-            while True:
-                msg = client.recv(self.BUFSIZ)
-                if msg != bytes("{quit}", "utf8"):
-                    self.broadcast(msg, name + ": ")
-                    self.broadcast_peers(msg, name + ": ")
-                else:
-                    # Will need to delete peer here from peer store
-                    try:
-                        client.send(bytes("{quit}", "utf8"))
-                    except ConnectionResetError:
-                        self.userquit(name)
-                    client.close()
-                    del Node.p2p.clients[client]
-                    self.userquit(name)
-                    break
-
-        def userquit(self, name):
-            # Nodes need to note that a peer has left still, and remove it from their peer store
-            self.broadcast(bytes("%s has left the chat." % name, "utf8"))
-            self.broadcast_peers(bytes("%s has left the chat." % name, "utf8"))
-            print("{0} has quit.".format(name))
-
-        def broadcast(self, msg, prefix=""):
-            # prefix is for name identification.
-            msg = prefix + msg.decode('utf8') + "|Peers;{0}".format(",".join(Node.p2p.peers))
-            print("broadcasting to clients {0}".format(str(Node.p2p.clients)))
-            for sock in Node.p2p.clients:
-                try:
-                    sock.send(bytes(msg, "utf8"))
-                except ConnectionResetError:
-                    print("Cannot send to ",prefix," Connection has been reset")
-        
-        def broadcast_peers(self, msg, prefix=""):
-            # prefix is for name identification.
-            send_peers = list(Node.p2p.peers) + [self.ADDR[0] + ":" + str(self.ADDR[1])]
-            msg = prefix + msg.decode('utf8') + "|Peers2;{0}".format(",".join(send_peers))
-            print("broadcasting to peers {0}".format(str(send_peers)))
-            for addr in Node.p2p.peers:
-                try:
-                    ip, port = addr.split(':')
-                    port = int(port)
-                    sock = socket(AF_INET, SOCK_STREAM)
-                    sock.connect((ip, port))
-                    sock.send(bytes(msg, "utf8"))
-                    sock.close()
-                except ConnectionResetError:
-                    print("Cannot send to ",prefix," Connection has been reset")
-
-        def listen(self):
-            self.SERVER.listen()
-            print("Waiting for connection...")
-            ACCEPT_THREAD = Thread(target = self.accept_incoming_connections)
-            ACCEPT_THREAD.start()
-            ACCEPT_THREAD.join()
-            self.SERVER.close()
-
-        def __init__(self, server_addr):
-            #self.clients = {}
-            self.addresses = {}
-
-            HOST, PORT = server_addr.split(":")
-            PORT = int(PORT)
-            self.BUFSIZ = 1024
-            self.ADDR = (HOST, PORT)
-
-            self.SERVER = socket(AF_INET, SOCK_STREAM)
-            self.SERVER.bind(self.ADDR)
-
-            Thread(target=self.listen).start()
-
-    class Client:
-        def receive(self):
-            """Handles receiving of messages."""
-            while True:
-                try:
-                    # client, client_addr = self.client_socket.accept()
-                    # if not Node.p2p.clients.contains(client):
-                    #     Node.p2p.clients[client] = client_addr
-                    msg = self.client_socket.recv(self.BUFSIZ).decode("utf8")
-                    
-                    # Parse out the peer list from the first message.  this could be cleaned up to check less ofter
-                    print(msg)
-                    fcon = msg.split("|")
-                    if(len(fcon) > 1):
-                        pcon = fcon[1].split(';')
-                        if (len(pcon) > 0 and pcon[0] == "Peers"):
-                            peers = pcon[1].split(",")
-                            for p in peers:
-                                if p != '':
-                                    #print("adding peer {0}".format(str(p)))
-                                    Node.p2p.addPeer(p)
-                    print(fcon[0])
-                    print(Node.p2p.peers)
-                except OSError:  # Possibly client has left the chat.
-                    break
-
-
-        def send(self, event=None):  # event is passed by binders.
-            """Handles sending of messages."""
-            while True:
-                msg = input("Get Input: ")
-                self.client_socket.send(bytes(msg, "utf8"))
-                if msg == "{quit}":
-                    self.client_socket.close()
-                    break
-
-        #----Now comes the sockets part----
-        #HOST = input('Enter host: ')
-        def __init__(self, client_addr):
-            self.client_socket = None
-            
-            self.BUFSIZ = 1024
-            self.addSocket(client_addr)
-
-        def addSocket(self, client_addr):
-            HOST, PORT = client_addr.split(":")
-            PORT = int(PORT)
-            ADDR = (HOST, PORT)
-            self.client_socket = socket(AF_INET, SOCK_STREAM)
-            self.client_socket.connect(ADDR)
-
-            Thread(target=self.send).start()
-            receive_thread = Thread(target=self.receive)
-            receive_thread.start()
-
     def start_server(self, server_addr):
         self.server_addr = server_addr
         HOST, PORT = self.server_addr.split(":")
@@ -198,12 +36,12 @@ class Node:
             Thread(target=self.handle_client, args=(client,)).start()
 
     def handle_client(self, client):  # Takes client socket as argument.
-        print(client)
+        #print(client)
         data = client.recv(self.BUFSIZ).decode("utf8")
-        print("handle string: {0} end".format(data))
+        #print("handle string: {0} end".format(data))
 
         if (data[0] == '\x11'):
-            print("register")
+            #print("register")
             check, addr, name = data.split('|')
             if (check != "\x11{Register}" and (addr != self.server_addr)):
                 client.send(bytes("\x10Bad friend, you have to register yourself.", "utf8"))
@@ -216,7 +54,7 @@ class Node:
             time.sleep(1)
             self.broadcast(bytes(msg, "utf8"))
         elif (data[0] == '\x12'):
-            print("peers: " + str(data))
+            #print("peers: " + str(data))
             check = data.split('|')[0]
             # print(check)
             if (check == "\x12{Peers}"):
@@ -224,12 +62,12 @@ class Node:
                 added = self.update_peers(data)
                 if added:
                     self.broadcast(bytes(data, 'utf8'))
-            print("Connections: {0}".format(str(self.connections)))
+            #print("Connections: {0}".format(str(self.connections)))
         x = 10
-        while x > 0:
+        while True: # x > 0:
             x = x - 1
             msg = client.recv(self.BUFSIZ)
-            print("RecieveMsg: " + msg.decode('utf8'))
+            #print("RecieveMsg: " + msg.decode('utf8'))
             if msg != bytes("\x13{quit}", "utf8"):
                 print(msg.decode('utf8'))
                 msgDecode = msg.decode('utf8')
@@ -249,14 +87,14 @@ class Node:
                 break
 
     def update_peers(self, peer_str):
-        print(peer_str)
+        #print(peer_str)
         peers_csv = peer_str.split("|")[1]
         peers = list(peers_csv.split(","))
         added = False
         for peer in peers:
-            print(peer)
+            #print(peer)
             name, ip, port = peer.split(":")
-            print("{0}, {1}, {2}".format(name, ip, port))
+            #print("{0}, {1}, {2}".format(name, ip, port))
             addr = ip + ":" + port
             if (name not in self.connections and addr != self.server_addr):
                 self.register_conn(addr, name)
@@ -280,7 +118,7 @@ class Node:
         sender = list(map(lambda x: x.strip(), sender))
         for name in self.connections:
             if (self.connections[name][0] != self.SERVER_ADDR and str(name) not in sender):
-                print("Broadcast: {3}|{0}|{1}|{2}|{4}".format(str(self.connections[name][0]), str(self.SERVER_ADDR), sender, name, str(name) not in sender))
+                #print("Broadcast: {3}|{0}|{1}|{2}|{4}".format(str(self.connections[name][0]), str(self.SERVER_ADDR), sender, name, str(name) not in sender))
                 try:
                     self.connections[name][1].send(bytes(prefix, "utf8") + msg)
                 except ConnectionResetError:
@@ -310,7 +148,7 @@ class Node:
         welcome_msg = self.client_sock.recv(self.BUFSIZ).decode('utf8')
         self.name = input(welcome_msg)
         register_msg = bytes("\x11{Register}" + "|{0}:{1}|{2}".format(self.SERVER_ADDR[0], self.SERVER_ADDR[1], self.name), 'utf8')
-        print("Client Register Msg: " + self.name)
+        #print("Client Register Msg: " + self.name)
         self.client_sock.send(register_msg)
         self.input_loop()
 
